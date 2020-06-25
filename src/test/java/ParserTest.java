bf1b67420de86beb260a765a04d00dff0c784837
tkachenkoanastazi <58345303+tkachenkoanastazi@users.noreply.github.com>
2020-06-25 20:47:35 +0300

    #1 popitka 5

--- src/test/java/ParserTest.java
+++ src/test/java/ParserTest.java
@@ -1,11 +1,14 @@
+import org.junit.Rule;
 import org.junit.jupiter.api.Test;
+import org.junit.rules.ExpectedException;
 
 import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ParserTest {
-
+    @Rule
+    public ExpectedException thrown= ExpectedException.none();
     Parser parser = new Parser();
 
     @Test
